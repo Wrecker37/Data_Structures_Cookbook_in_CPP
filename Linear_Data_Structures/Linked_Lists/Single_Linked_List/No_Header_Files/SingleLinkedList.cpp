290ca2c987f26cc1f1f1b037cbfeae5b5a390d11
Wrecker37 <jairo.pedroza78@gmail.com>
2022-07-14 17:20:49 -0500

    SLL file in progress

--- Linear_Data_Structures/Linked_Lists/Single_Linked_List/No_Header_Files/SingleLinkedList.cpp
+++ Linear_Data_Structures/Linked_Lists/Single_Linked_List/No_Header_Files/SingleLinkedList.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
 
-struct Node{
+class Node{
+public:
     int data;
-    struct Node *next;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+
 
 };
 
-struct SLL{
+class SLL{
     Node *head;
+
+    public:
+        SLL(){
+            this->head = nullptr;
+        }
+        void insertNode(int data){
+            Node *newNode = new Node(data);
+            if(this->head == nullptr){
+                this->head = newNode;
+            }else{
+                Node *temp = head;
+                while(temp->next != nullptr){
+                    temp = temp->next;
+                }
+                temp->next = newNode;
+            }
+            
+        }
 
 
 };
