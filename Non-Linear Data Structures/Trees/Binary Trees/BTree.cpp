a7c03607934fac638b2e74432bfe1e89eba6536c
Wrecker37 <jairo.pedroza78@gmail.com>
2022-07-24 11:57:47 -0500

    added traversal functions

--- Non-Linear Data Structures/Trees/Binary Trees/BTree.cpp
+++ Non-Linear Data Structures/Trees/Binary Trees/BTree.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 class BNode{
 public:
     int data;
@@ -9,5 +11,51 @@ public:
         this->right = nullptr;
     }
 
+};
+
+class BTree{
+public:
+    BNode *root;
+    
+    BTree(int data){
+        BNode *newNode = new BNode(data);
+        this->root = newNode;
+    }
+
+    void preOrder(BNode *root){
+        if(root != nullptr){
+
+            std::cout << root->data << " ";
+            preOrder(root->left);
+            preOrder(root->right);
+        }
+    }
+
+    void inOrder(BNode *root){
+        inOrder(root->left);
+        std::cout << root->data <<  " ";
+        inOrder(root->right);
+    }
+
+    void postOrder(BNode *root){
+        postOrder(root->left);
+        postOrder(root->right);
+        std::cout << root->data << " ";
+    }
+
 
 };
+
+int main(){
+
+    BTree tree(1);
+    tree.root->left = new BNode(2);
+    tree.root->right = new BNode(3);
+
+    tree.root->left->left = new BNode(4);
+    tree.root->left->right = new BNode(5);
+    tree.root->right->left = new BNode(6);
+    tree.root->right->right = new BNode(7);
+
+    return 0;
+}
